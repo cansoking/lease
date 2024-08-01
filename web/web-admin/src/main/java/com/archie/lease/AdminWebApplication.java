423e3d8ffc1a8d33d4f53a58056f6410c366ecdd
ArchieWang <cansoking@gmail.com>
2024-08-01 23:48:03 +0800

    Git绑定测试

--- web/web-admin/src/main/java/com/archie/lease/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/archie/lease/AdminWebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminWebApplication {
     public static void main(String[] args) {
+        // 测试git
         SpringApplication.run(AdminWebApplication.class, args);
     }
 }
