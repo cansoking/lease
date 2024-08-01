b7c408de22bf0d8411e8c604336b6d84372490e0
cansoking <cansoking@gmail.com>
2024-08-01 23:51:43 +0800

    Git绑定测试2

--- web/web-admin/src/main/java/com/archie/lease/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/archie/lease/AdminWebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminWebApplication {
     public static void main(String[] args) {
-        // 测试git
+        // 测试git2
         SpringApplication.run(AdminWebApplication.class, args);
     }
 }
