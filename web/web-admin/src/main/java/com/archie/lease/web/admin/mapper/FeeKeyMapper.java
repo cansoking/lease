d14b64e8767fa45a658f1329c0eea4a06e1b54ac
ArchieWang <cansoking@hotmail.com>
2024-08-08 21:28:59 +0800

    1、实现费用管理 2、实现minio的图片上传 3、实现地区接口

--- web/web-admin/src/main/java/com/archie/lease/web/admin/mapper/FeeKeyMapper.java
+++ web/web-admin/src/main/java/com/archie/lease/web/admin/mapper/FeeKeyMapper.java
@@ -1,7 +1,10 @@
 package com.archie.lease.web.admin.mapper;
 
 import com.archie.lease.model.entity.FeeKey;
+import com.archie.lease.web.admin.vo.fee.FeeKeyVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
 * @author liubo
@@ -11,6 +14,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 */
 public interface FeeKeyMapper extends BaseMapper<FeeKey> {
 
+    List<FeeKeyVo> listFeeInfo();
 }
 
 
--- web/web-admin/src/main/java/com/archie/lease/web/admin/service/FeeKeyService.java
+++ web/web-admin/src/main/java/com/archie/lease/web/admin/service/FeeKeyService.java
@@ -1,7 +1,10 @@
 package com.archie.lease.web.admin.service;
 
 import com.archie.lease.model.entity.FeeKey;
+import com.archie.lease.web.admin.vo.fee.FeeKeyVo;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
 * @author liubo
@@ -10,4 +13,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface FeeKeyService extends IService<FeeKey> {
 
+    List<FeeKeyVo> listFeeInfo();
 }
