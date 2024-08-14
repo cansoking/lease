dcddba1c1e53a88b8568f2532a25bbe4e02e3f0d
ArchieWang <cansoking@hotmail.com>
2024-08-14 20:13:58 +0800

    完成平台用户管理接口

--- web/web-admin/src/main/java/com/archie/lease/web/admin/controller/user/UserInfoController.java
+++ web/web-admin/src/main/java/com/archie/lease/web/admin/controller/user/UserInfoController.java
@@ -2,12 +2,16 @@ package com.archie.lease.web.admin.controller.user;
 
 
 import com.archie.lease.common.result.Result;
+import com.archie.lease.web.admin.service.UserInfoService;
 import com.archie.lease.web.admin.vo.user.UserInfoQueryVo;
 import com.archie.lease.model.entity.UserInfo;
 import com.archie.lease.model.enums.BaseStatus;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "用户信息管理")
@@ -15,15 +19,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/user")
 public class UserInfoController {
 
+    @Autowired
+    private UserInfoService service;
+
     @Operation(summary = "分页查询用户信息")
     @GetMapping("page")
     public Result<IPage<UserInfo>> pageUserInfo(@RequestParam long current, @RequestParam long size, UserInfoQueryVo queryVo) {
-        return Result.ok();
+        Page<UserInfo> page = new Page<>(current, size);
+        IPage<UserInfo> result = service.listByqueryVo(page, queryVo);
+        return Result.ok(result);
     }
 
     @Operation(summary = "根据用户id更新账号状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam BaseStatus status) {
+        LambdaUpdateWrapper<UserInfo> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(UserInfo::getId, id).set(UserInfo::getStatus, status);
+        service.update(wrapper);
         return Result.ok();
     }
 }
--- web/web-admin/src/main/java/com/archie/lease/web/admin/service/impl/UserInfoServiceImpl.java
+++ web/web-admin/src/main/java/com/archie/lease/web/admin/service/impl/UserInfoServiceImpl.java
@@ -1,20 +1,31 @@
 package com.archie.lease.web.admin.service.impl;
 
 import com.archie.lease.web.admin.mapper.UserInfoMapper;
+import com.archie.lease.web.admin.vo.user.UserInfoQueryVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.archie.lease.model.entity.UserInfo;
 import com.archie.lease.web.admin.service.UserInfoService;
 import org.springframework.stereotype.Service;
 
 /**
-* @author liubo
-* @description 针对表【user_info(用户信息表)】的数据库操作Service实现
-* @createDate 2023-07-24 15:48:00
-*/
+ * @author liubo
+ * @description 针对表【user_info(用户信息表)】的数据库操作Service实现
+ * @createDate 2023-07-24 15:48:00
+ */
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo>
-    implements UserInfoService{
+        implements UserInfoService {
 
+    @Override
+    public IPage<UserInfo> listByqueryVo(Page<UserInfo> page, UserInfoQueryVo queryVo) {
+        LambdaQueryWrapper<UserInfo> wrapper = new LambdaQueryWrapper<>();
+        wrapper.like(queryVo.getPhone() != null, UserInfo::getPhone, queryVo.getPhone())
+                .eq(queryVo.getStatus() != null, UserInfo::getStatus, queryVo.getStatus());
+        return super.page(page, wrapper);
+    }
 }
 
 
